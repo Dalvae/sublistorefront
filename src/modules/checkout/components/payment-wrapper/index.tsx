dfc0a0df6cc996484333cb167983291d276c01cb
Dalvae <diego.alvarez.e@ug.uchile.cl>
2023-12-01 18:09:34 -0300

    removed stripe

--- src/modules/checkout/components/payment-wrapper/index.tsx
+++ src/modules/checkout/components/payment-wrapper/index.tsx
@@ -1,32 +1,37 @@
-// import { PaymentSession } from "@medusajs/medusa"
+import { PaymentSession } from "@medusajs/medusa"
 // import { Elements } from "@stripe/react-stripe-js"
 // import { loadStripe, StripeElementsOptions } from "@stripe/stripe-js"
-// import React from "react"
+import React from "react"
 
-// type WrapperProps = {
-//   paymentSession?: PaymentSession | null
-// }
+type WrapperProps = {
+  paymentSession?: PaymentSession | null
+}
 
-// const Wrapper: React.FC<WrapperProps> = ({ paymentSession, children }) => {
-//   if (!paymentSession) {
-//     return <div>{children}</div>
-//   }
+const Wrapper: React.FC<WrapperProps> = ({ paymentSession, children }) => {
+  if (!paymentSession) {
+    return <div>{children}</div>
+  }
 
-//   switch (paymentSession.provider_id) {
-//     case "stripe":
-//       return (
-//         <StripeWrapper paymentSession={paymentSession}>
-//           {children}
-//         </StripeWrapper>
-//       )
+  // Comentado el caso de Stripe, puedes agregar otros casos aquí si es necesario.
+  // switch (paymentSession.provider_id) {
+  //   case "stripe":
+  //     return (
+  //       <StripeWrapper paymentSession={paymentSession}>
+  //         {children}
+  //       </StripeWrapper>
+  //     );
 
-//     default:
-//       return <div>{children}</div>
-//   }
-// }
+  //   default:
+  //     return <div>{children}</div>;
+  // }
 
-// const stripeKey = process.env.NEXT_PUBLIC_STRIPE_KEY
-// const stripePromise = stripeKey ? loadStripe(stripeKey) : null
+  // Retornando simplemente los hijos si no hay una lógica específica para un proveedor de pago.
+  return <div>{children}</div>
+}
+
+// Comentado el componente StripeWrapper y la lógica relacionada con Stripe.
+// const stripeKey = process.env.NEXT_PUBLIC_STRIPE_KEY;
+// const stripePromise = stripeKey ? loadStripe(stripeKey) : null;
 
 // const StripeWrapper: React.FC<WrapperProps> = ({
 //   paymentSession,
@@ -34,17 +39,17 @@
 // }) => {
 //   const options: StripeElementsOptions = {
 //     clientSecret: paymentSession!.data.client_secret as string | undefined,
-//   }
+//   };
 
 //   if (!stripePromise) {
-//     return <div>{children}</div>
+//     return <div>{children}</div>;
 //   }
 
 //   return (
 //     <Elements stripe={stripePromise} options={options}>
 //       {children}
 //     </Elements>
-//   )
-// }
+//   );
+// };
 
-// export default Wrapper
+export default Wrapper
