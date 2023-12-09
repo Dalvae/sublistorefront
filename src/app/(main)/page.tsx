f7bddf75436442d5f279c2ef2f297d10f9325d33
Dalvae <diego.alvarez.e@ug.uchile.cl>
2023-12-09 00:02:18 -0300

    reverted test, for displaying

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -11,14 +11,14 @@ export const metadata: Metadata = {
 }
 
 export default async function Home() {
-  // // const { collections, count } = await getCollectionsList(0, 3)
+  const { collections, count } = await getCollectionsList(0, 3)
 
   return (
     <>
       <Hero />
-      {/* <Suspense fallback={<SkeletonHomepageProducts count={count} />}>
+      <Suspense fallback={<SkeletonHomepageProducts count={count} />}>
         <FeaturedProducts collections={collections} />
-      </Suspense> */}
+      </Suspense>
     </>
   )
 }
