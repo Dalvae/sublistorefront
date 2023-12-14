41b3f1ea91bdf4837f47c4c1cbe300fbbd3d406b
Dalvae <diego.alvarez.e@ug.uchile.cl>
2023-12-14 11:42:59 -0300

    new Test

--- src/modules/checkout/components/payment-test-webpay/index.tsx
+++ src/modules/checkout/components/payment-test-webpay/index.tsx
@@ -1,18 +1,32 @@
 import React from "react"
+import { PaymentSession } from "@medusajs/medusa"
+import { useCart } from "medusa-react"
 
 const TestWebpayButton = () => {
-  const handleWebpayTest = async () => {
-    // Simula la llamada a tu procesador de pagos
-    // Reemplaza esto con la lógica real si es posible
-    const fakeResponse = {
-      url: "https://webpay.example.com/payment-url",
-      token: "some-fake-token",
-    }
+  const { cart } = useCart() // Use the useCart hook to access the cart context
 
-    console.log("Webpay Response:", fakeResponse)
+  const handleWebpayTest = async () => {
+    // Check if the cart and payment session data exist
+    if (cart && cart.payment_session && cart.payment_session.data) {
+      // Assuming transbankToken is part of the payment session data
+      const transbankToken = cart.payment_session.data.transbankToken
+      const redirectUrl = cart.payment_session.data.redirectUrl
+
+      if (transbankToken) {
+        console.log("Transbank Token:", transbankToken)
+      } else {
+        console.log("Transbank Token not found in payment session data")
+      }
+
+      if (redirectUrl) {
+        console.log("Redirect URL:", redirectUrl)
+      } else {
+        console.log("Redirect URL not found in payment session data")
+      }
+    } else {
+      console.log("Cart or Payment Session Data is unavailable")
+    }
   }
 
   return <button onClick={handleWebpayTest}>Test Webpay Payment</button>
 }
-
-export default TestWebpayButton
