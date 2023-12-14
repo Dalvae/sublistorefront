d221976db025d01bc987b1144d1b407e14c65907
Dalvae <diego.alvarez.e@ug.uchile.cl>
2023-12-14 18:09:42 -0300

    Reverted the console

--- src/modules/checkout/components/payment-button/index.tsx
+++ src/modules/checkout/components/payment-button/index.tsx
@@ -224,4 +224,60 @@ const ManualTestPaymentButton = ({ notReady }: { notReady: boolean }) => {
   )
 }
 
+const WebpayPaymentButton = ({
+  session,
+  notReady,
+}: {
+  session: PaymentSession
+  notReady: boolean
+}) => {
+  const [submitting, setSubmitting] = useState(false)
+  const [errorMessage, setErrorMessage] = useState<string | undefined>(
+    undefined
+  )
+
+  const { cart } = useCart()
+  const { onPaymentCompleted } = useCheckout()
+
+  const handlePayment = async (
+    _data: OnApproveData,
+    actions: OnApproveActions
+  ) => {
+    actions?.order
+      ?.authorize()
+      .then((authorization) => {
+        if (authorization.status !== "COMPLETED") {
+          setErrorMessage(`An error occurred, status: ${authorization.status}`)
+          return
+        }
+        onPaymentCompleted()
+      })
+      .catch(() => {
+        setErrorMessage(`An unknown error occurred, please try again.`)
+      })
+      .finally(() => {
+        setSubmitting(false)
+      })
+  }
+  return (
+    <PayPalScriptProvider
+      options={{
+        "client-id": PAYPAL_CLIENT_ID,
+        currency: cart?.region.currency_code.toUpperCase(),
+        intent: "authorize",
+      }}
+    >
+      {errorMessage && (
+        <span className="text-rose-500 mt-4">{errorMessage}</span>
+      )}
+      <PayPalButtons
+        style={{ layout: "horizontal" }}
+        createOrder={async () => session.data.id as string}
+        onApprove={handlePayment}
+        disabled={notReady || submitting}
+      />
+    </PayPalScriptProvider>
+  )
+}
+
 export default PaymentButton
