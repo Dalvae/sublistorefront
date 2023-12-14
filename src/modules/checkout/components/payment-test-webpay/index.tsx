a98f3c51fa12d195c062f21545b74dc4a987eaf2
Dalvae <diego.alvarez.e@ug.uchile.cl>
2023-12-14 12:42:14 -0300

    New test button

--- src/modules/checkout/components/payment-test-webpay/index.tsx
+++ src/modules/checkout/components/payment-test-webpay/index.tsx
@@ -1,34 +1,49 @@
-import React from "react"
-import { PaymentSession } from "@medusajs/medusa"
+import React, { useState } from "react"
 import { useCart } from "medusa-react"
 
+// Definiendo una interfaz para los datos de Transbank
+interface TransbankData {
+  token: string
+  url: string
+}
+
 const TestWebpayButton = () => {
-  const { cart } = useCart() // Use the useCart hook to access the cart context
+  const { cart } = useCart()
+  const [transbankData, setTransbankData] = useState<TransbankData | null>(null)
 
   const handleWebpayTest = async () => {
-    // Check if the cart and payment session data exist
     if (cart && cart.payment_session && cart.payment_session.data) {
-      // Assuming transbankToken and redirectUrl are part of the payment session data
       const transbankToken = cart.payment_session.data.transbankToken
       const redirectUrl = cart.payment_session.data.redirectUrl
 
-      if (transbankToken) {
-        console.log("Transbank Token:", transbankToken)
+      if (!!transbankToken && redirectUrl) {
+        setTransbankData({ token: transbankToken, url: redirectUrl })
       } else {
-        console.log("Transbank Token not found in payment session data")
-      }
-
-      if (redirectUrl) {
-        console.log("Redirect URL:", redirectUrl)
-      } else {
-        console.log("Redirect URL not found in payment session data")
+        console.log(
+          "Datos de Transbank o URL de redireccionamiento no encontrados"
+        )
       }
     } else {
-      console.log("Cart or Payment Session Data is unavailable")
+      console.log("Datos del carrito o de la sesión de pago no disponibles")
     }
   }
 
-  return <button onClick={handleWebpayTest}>Test Webpay Payment</button>
+  return (
+    <>
+      <button
+        onClick={handleWebpayTest}
+        style={{ backgroundColor: "#561456", color: "white" }}
+      >
+        Test Webpay Payment
+      </button>
+      {transbankData && (
+        <form method="post" action={transbankData.url}>
+          <input type="hidden" name="token_ws" value={transbankData.token} />
+          <input type="submit" value="Ir a pagar" />
+        </form>
+      )}
+    </>
+  )
 }
 
 export default TestWebpayButton
