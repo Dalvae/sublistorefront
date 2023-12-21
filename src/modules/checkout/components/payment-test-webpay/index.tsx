2099adc5490e39c65f52c3d422167f5b61e83a12
Dalvae <diego.alvarez.e@ug.uchile.cl>
2023-12-21 10:17:56 -0300

    new window approach

--- src/modules/checkout/components/payment-test-webpay/index.tsx
+++ src/modules/checkout/components/payment-test-webpay/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react"
 import { useCart } from "medusa-react"
-import { useRouter } from "next/router"
 
 // Define una interfaz para los datos de Transbank
 interface TransbankData {
@@ -12,40 +11,37 @@ interface TransbankData {
 const WebpayButton = () => {
   const { cart } = useCart()
   const [transbankData, setTransbankData] = useState<TransbankData | null>(null)
-  const router = useRouter() // Utiliza useRouter para acceder a la URL actual y sus parámetros
 
   useEffect(() => {
     // Verificar si estamos en el lado del cliente
-    if (router.isReady) {
-      if (typeof window !== "undefined") {
-        const queryParams = new URLSearchParams(window.location.search)
-        const tokenWs = queryParams.get("token_ws")
+    if (typeof window !== "undefined") {
+      const queryParams = new URLSearchParams(window.location.search)
+      const tokenWs = queryParams.get("token_ws")
 
-        if (tokenWs) {
-          console.log("Token WS recibido:", tokenWs)
-          // Procesar el token_ws y confirmar la transacción
-        } else if (cart?.payment_session?.data) {
-          // Carga los datos de Transbank si el token_ws no está presente
-          const transbankData = cart.payment_session.data
-          const transbankToken =
-            typeof transbankData.transbankToken === "string"
-              ? transbankData.transbankToken
-              : ""
-          const redirectUrl =
-            typeof transbankData.redirectUrl === "string"
-              ? transbankData.redirectUrl
-              : ""
-          const buyOrder =
-            typeof transbankData.buyOrder === "string"
-              ? transbankData.buyOrder
-              : ""
+      if (tokenWs) {
+        console.log("Token WS recibido:", tokenWs)
+        // Aquí puedes hacer algo con el token_ws
+      } else if (cart?.payment_session?.data) {
+        // Carga los datos de Transbank si el token_ws no está presente
+        const transbankData = cart.payment_session.data
+        const transbankToken =
+          typeof transbankData.transbankToken === "string"
+            ? transbankData.transbankToken
+            : ""
+        const redirectUrl =
+          typeof transbankData.redirectUrl === "string"
+            ? transbankData.redirectUrl
+            : ""
+        const buyOrder =
+          typeof transbankData.buyOrder === "string"
+            ? transbankData.buyOrder
+            : ""
 
-          setTransbankData({
-            token: transbankToken,
-            url: redirectUrl,
-            buyOrder,
-          })
-        }
+        setTransbankData({
+          token: transbankToken,
+          url: redirectUrl,
+          buyOrder,
+        })
       }
     }
   }, [cart])
