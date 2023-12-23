c16c31947b600bc2958f239eb76f5fc48f1a36f4
Dalvae <diego.alvarez.e@ug.uchile.cl>
2023-12-23 14:49:02 -0300

    change dependecys of hooks

--- src/lib/hooks/use-changepayment-data.tsx
+++ src/lib/hooks/use-changepayment-data.tsx
@@ -28,7 +28,7 @@ const useCheckoutActions = () => {
         console.error("Error al manejar la respuesta de Transbank:", error)
       }
     }
-  }, [cart, updatePaymentSession])
+  }, [updatePaymentSession])
 
   return {
     handleTransbankResponse,
