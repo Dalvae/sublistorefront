2cdef8aa038d4850599e9fa2457b99fe195c9756
Dalvae <diego.alvarez.e@ug.uchile.cl>
2023-12-30 16:49:40 -0300

    logo to left

--- src/modules/layout/components/side-menu/index.tsx
+++ src/modules/layout/components/side-menu/index.tsx
@@ -30,8 +30,8 @@ const SideMenu = ({ searchModalOpen }: { searchModalOpen: () => void }) => {
             <>
               <div className="relative flex h-full">
                 <Popover.Button className="relative h-full flex items-center transition-all ease-out duration-200 focus:outline-none hover:text-ui-fg-base">
-                  Menu
                   <Logo />
+                  Menu
                 </Popover.Button>
               </div>
 
