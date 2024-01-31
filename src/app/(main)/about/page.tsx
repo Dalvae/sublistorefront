2571d8ea9b7bb2cdc5c2a9b7a19d61e2bfc0e492
Dalvae <diego.alvarez.e@ug.uchile.cl>
2024-01-31 18:49:00 -0300

    added p

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -27,20 +27,27 @@ export default function About() {
             Se me hizo fácil inclinarme por este nombre, pues incluye mis
             iniciales y las iniciales de Casablanca, comuna de la V región con
             el valle más hermoso del mundo y que nos acogió hace algunos años.
-            La &quot;magia&quot; que se puede inferir de sus letras es lo que me
-            inspiró, pues mi objetivo es añadir un toque mágico a cada producto,
-            convirtiéndolo en algo único y especial, porque en Sublimahyca
-            creemos que la magia de los regalos es que sean únicos y especiales
-            para cada persona. Desde la selección cuidadosa de materiales hasta
-            la atención meticulosa a los detalles, cada artículo que entregamos
-            es una expresión de calidad y dedicación. Nos esforzamos por ofrecer
-            no solo productos personalizados, sino experiencias que te hagan
-            sonreír cada vez que los utilizas. En SUBLIMAHYCA, tu creatividad es
-            nuestra guía para que &quot;magia&quot; no sea solo una palabra si
-            no también un compromiso de que cada creación lleva consigo un
-            diseño para inspirar sonrisas y crear recuerdos duraderos. Te
-            invitamos a conocer nuestro trabajo. ¡Bienvenido a SUBLIMAHYCA,
-            donde la magia se convierte en realidad!
+            <p className=" text-md text-gray-700 self-center text-justify mt-1">
+              {" "}
+              La &quot;magia&quot; que se puede inferir de sus letras es lo que
+              me inspiró, pues mi objetivo es añadir un toque mágico a cada
+              producto, convirtiéndolo en algo único y especial, porque en
+              Sublimahyca creemos que la magia de los regalos es que sean únicos
+              y especiales para cada persona. Desde la selección cuidadosa de
+              materiales hasta la atención meticulosa a los detalles, cada
+              artículo que entregamos es una expresión de calidad y dedicación.
+              Nos esforzamos por ofrecer no solo productos personalizados, sino
+              experiencias que te hagan sonreír cada vez que los utilizas.
+            </p>
+            <p className=" text-md text-gray-700 self-center text-justify mt-1">
+              {" "}
+              En SUBLIMAHYCA, tu creatividad es nuestra guía para que
+              &quot;magia&quot; no sea solo una palabra si no también un
+              compromiso de que cada creación lleva consigo un diseño para
+              inspirar sonrisas y crear recuerdos duraderos. Te invitamos a
+              conocer nuestro trabajo. ¡Bienvenido a SUBLIMAHYCA, donde la magia
+              se convierte en realidad!
+            </p>
           </p>
         </div>
         <img
