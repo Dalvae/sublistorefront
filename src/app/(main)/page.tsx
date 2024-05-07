4dfffe2d6aea30df84f4ab4b9fe586eedf8b1e8e
Dalvae <diego.alvarez.e@ug.uchile.cl>
2024-05-06 20:44:27 -0400

    All products on home and more products per column

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -4,20 +4,21 @@ import Hero from "@modules/home/components/hero"
 import SkeletonHomepageProducts from "@modules/skeletons/components/skeleton-homepage-products"
 import { Metadata } from "next"
 import { Suspense } from "react"
-
+import StoreTemplate from "@modules/store/templates"
 export const metadata: Metadata = {
   title: "Home",
   description: "Sublimahyca, tienda sublimación y regalos personalizados ",
 }
 
 export default async function Home() {
-  const { collections, count } = await getCollectionsList(0, 3)
+  const { collections, count } = await getCollectionsList(0, 20)
 
   return (
     <>
       <Hero />
       <Suspense fallback={<SkeletonHomepageProducts count={count} />}>
-        <FeaturedProducts collections={collections} />
+        <StoreTemplate />
+        {/* <FeaturedProducts collections={collections} /> */}
       </Suspense>
     </>
   )
